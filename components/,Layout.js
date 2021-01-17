c8dfa12209b482802df6afdb67825241bbc0a0bd
Bryan Field <musicwithoutpaper@gmail.com>
2021-01-16 22:26:19 -0600

    Some header and footer and CSS

--- components/,Layout.js
+++ components/,Layout.js
@@ -14,6 +14,32 @@ export default function Layout({ children }) {
       <link rel="icon" href="/favicon.ico" />
       <meta property="og:image" content="/_next/image?url=/favisplash.png&w=1920&q=75" />
     </Head>
+    <header>
+      <div className="the-width">
+        <img src="/_next/image?url=https://700software.com/bryan.jpg&w=640&q=75" id="head-me-circle" />
+        <h1>stocks.700Software.com</h1>
+        <p>Sample project</p>
+      </div>
+    </header>
     {children}
+    <footer>
+      <a href="https://github.com/700software/" target="_blank">
+        <span className="material-icons">code</span>
+        <span> </span>
+        Source
+        </a>
+      <>       </>
+      <a href="https://700software.com" target="_blank">
+        <span className="material-icons">home</span>
+        <span> </span>
+        Resume
+      </a>
+      <>       </>
+      <span className="ib">
+        <span className="material-icons">phone</span>
+        <> </>
+        423-802-8971
+      </span>
+    </footer>
   </>
 }
