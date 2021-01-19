6634600cd1f648e7a09b57b825115743be51bac5
Bryan Field <musicwithoutpaper@gmail.com>
2021-01-18 21:08:50 -0600

    Drop-in roundC, round and commaize functions
    from https://github.com/Skewjo/SysLat_Website/blob/83cc33c5db07e7b23d26346713ee5a62b8ad3e8c/lib/%2Cglobal.js#L43-L83

--- lib/util.ts
+++ lib/util.ts
@@ -54,3 +54,44 @@ export function paramE(x) {
     .replace(/%7C/g, '|')
     .replace(/%7D/g, '}')
 }
+
+/**
+ * Round a string that had user input with commas potentially
+ * @param {string | number} x 
+ * @param {number?} digits max decimal digits rounding
+ * @param {number?} minDigits min digits to display even if .00
+ * @returns {number}
+ */
+export function roundC(x, digits, minDigits) {
+  if (typeof x == 'string') x = x.replace(/,/g, '') // auto-strips comma
+  if (digits == null) return x * 1 // returns same number if no rounding specified
+  var x = round(x, digits, minDigits == null ? digits : minDigits) // returns string unless minDigits is zero
+  return x == 0 ? '' : x
+}
+
+/**
+* round without having micro rounding differences problems
+* @param {number} x 
+* @param {number?} digits max decimal digits rounding
+* @param {number?} minDigits min digits to display even if .00
+* @returns {number}
+*/
+export function round(x, digits, minDigits) {
+  x = x < 0 ? x - .000000001 : x*1 + .000000001 // no comma strip, but fixes micro-rounding
+  if (!digits && !minDigits) return Math.round(x) // more efficient when possible
+  x = x.toFixed(digits) // round
+  if (!minDigits) return x * 1 // returns number if no minDigits specified
+  if (digits == minDigits) return x
+  x = x * 1 + '' // remove trailing zeros
+  if (x.indexOf('e') != -1) return x // just in case
+  var idxNow = x.lastIndexOf('.')
+  if (idxNow == -1) x += '.'
+  else minDigits -= x.length - idxNow - 1 // minDigits becomes 'remaining digits to add'
+  while (minDigits-- > 0) x += '0' // returns string with correct minDigits as specified
+  return x
+}
+
+/** e.g. Converts 1000.1 to 1,000.1 */
+export function commaize(x) {
+  return (''+x).replace(/(\d)(?=(?:\d\d\d)+(?:\.|$))/g, '$1,').replace(/(\.\d*),/g, '$1')
+}
